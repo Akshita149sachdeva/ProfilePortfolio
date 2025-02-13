acce8b7b0ba2cea590d5cb800dd2c2c72a94a34d
Akshita149sachdeva <akshitasach149@gmail.com>
2025-02-13 12:41:09 +0530

    edits

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,14 +19,11 @@ const Navbar = () => {
           <a href="#">
             <img src="/facebook.png" alt="" />
           </a>
-          <a href="#">
+          <a href="https://www.instagram.com/akshita_sach/">
             <img src="/instagram.png" alt="" />
           </a>
           <a href="#">
             <img src="/youtube.png" alt="" />
-          </a>
-          <a href="#">
-            <img src="/dribbble.png" alt="" />
           </a>
         </div>
       </div>
